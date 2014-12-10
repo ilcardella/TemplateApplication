5821d1100412bb17f403f8da2a36b8df038b5b01
alberto cardellini <albe.carde@gmail.com>
2014-12-10 10:54:35 +0100

    first commit

---
+++ src/it/polimi/application/pluto/model/editor/Clock.java
@@ -0,0 +1,19 @@
+package it.polimi.application.pluto.model.editor;
+
+import it.polimi.application.pluto.model.Mission;
+
+
+public class Clock extends Node {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Mission run(Mission mission) {
+		// TODO Auto-generated method stub
+		return super.run(mission);
+	}
+
+}
---
+++ src/it/polimi/application/pluto/model/editor/ConditionBlock.java
@@ -0,0 +1,19 @@
+package it.polimi.application.pluto.model.editor;
+
+import it.polimi.application.pluto.model.Mission;
+
+
+
+public class ConditionBlock extends Node {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Mission run(Mission m) {
+		return null;
+	}
+
+}
