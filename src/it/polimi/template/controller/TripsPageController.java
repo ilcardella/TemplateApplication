23d7d6c94b1d485ccb13307d3791c263a0386af1
alberto cardellini <albe.carde@gmail.com>
2015-01-07 15:59:44 +0100

    jk

--- src/it/polimi/template/controller/TripsPageController.java
+++ src/it/polimi/template/controller/TripsPageController.java
@@ -13,7 +13,10 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetAdapter;
+import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,75 +35,64 @@ public class TripsPageController {
 		this.tripsPage = tripsPage;
 		this.items = items;
 		this.missionName = missionName;
-		
-		this.tripsPage.setDropTargetListener();
+
+		this.tripsPage.setDropTargetListener(new DragDropListener());
 
 	}
 
-	public class MyDropTargetListener extends DropTargetAdapter {
+	public void manageDragAndDrop() {
 
-		private DropTarget dropTarget;
-		private JLabel label;
+		// if drop event is ok, create the Trip and set the name
+		Trip trip = new Trip();
+		trip.setName(missionName + " - " + tripCounter);
+		tripCounter++;
 
-		public MyDropTargetListener(JLabel label) {
-			this.label = label;
-			dropTarget = new DropTarget(label, DnDConstants.ACTION_COPY, this,
-					true, null);
+		if (action.equals(Action.PICK_ITEM.toString())
+				|| action.equals(Action.RELEASE_ITEM.toString())) {
+
+			// create a string list with the name of the items
+			List<String> itemsNames = new ArrayList<String>();
+			for (Item i : items)
+				itemsNames.add(i.getName());
+
+			// get the items from the user input
+			String iName = tripsPage.showItemsPanel(itemsNames);
+
+			// set the item to the trip
+			for (Item i : items)
+				if (i.getName().equals(iName))
+					trip.setItem(i);
+
+			// set the priority to the trip
+			int priority = tripsPage.showPriorityPanel();
+			trip.setPriority(priority);
+
+			// set the delay
+			int delay = tripsPage.showDelayPanel();
+			trip.setDelay(delay);
+
 		}
+
+	}
+
+	class DragDropListener implements DropTargetListener {
 		
 		@Override
-		public void drop(DropTargetDropEvent event) {
-			try {
-				Point p = event.getLocation();
-				Transferable tr = event.getTransferable();
-
-				String action = (String) tr
-						.getTransferData(DataFlavor.stringFlavor);
-
-				if (event.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-					event.acceptDrop(DnDConstants.ACTION_COPY);
-					event.dropComplete(true);
-
-					// if drop event is ok, create the Trip and set the name
-					Trip trip = new Trip();
-					trip.setName(missionName + " - " + tripCounter);
-					tripCounter++;
-					
-					if( action.equals(Action.PICK_ITEM.toString()) || action.equals(Action.RELEASE_ITEM.toString()) ){
-						
-						// create a string list with the name of the items
-						List<String> itemsNames = new ArrayList<String>();
-						for(Item i: items)
-							itemsNames.add(i.getName());
-						
-						// get the items from the user input
-						String iName = tripsPage.showItemsPanel(itemsNames);
-						
-						// set the item to the trip
-						for(Item i: items)
-							if(i.getName().equals(iName))
-								trip.setItem(i);
-						
-						// set the priority to the trip
-						int priority = tripsPage.showPriorityPanel();
-						trip.setPriority(priority);
-						
-						// set the delay
-						int delay = tripsPage.showDelayPanel();
-						trip.setDelay(delay);
-						
-						
-						
-					}
-				}
-
-				event.rejectDrop();
-			} catch (Exception e) {
-				e.printStackTrace();
-				event.rejectDrop();
-			}
+		public void drop(DropTargetDropEvent arg0) {
 
 		}
+
+		@Override
+		public void dragEnter(DropTargetDragEvent arg0) {}
+
+		@Override
+		public void dragExit(DropTargetEvent arg0) {}
+
+		@Override
+		public void dragOver(DropTargetDragEvent arg0) {}
+
+		@Override
+		public void dropActionChanged(DropTargetDragEvent arg0) {}
 
 	}
 
