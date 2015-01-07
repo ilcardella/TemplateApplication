ffcca078b5c562a7be58fb45ee52116dc1320610
alberto cardellini <albe.carde@gmail.com>
2015-01-07 13:09:49 +0100

    added TripsController

---
+++ src/it/polimi/template/controller/TripsPageController.java
@@ -0,0 +1,15 @@
+package it.polimi.template.controller;
+
+import it.polimi.template.model.Item;
+import it.polimi.template.view.TripsPage;
+
+import java.util.List;
+
+public class TripsPageController {
+
+	public TripsPageController(TripsPage tripsPage, List<Item> items,
+			String missionName) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
