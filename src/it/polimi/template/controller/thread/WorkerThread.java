6ab1003db8b2fd0800617f45217fbb70d4ca0d6c
alberto cardellini <albe.carde@gmail.com>
2015-01-07 18:02:23 +0100

    sadsd

--- src/it/polimi/template/controller/thread/WorkerThread.java
+++ src/it/polimi/template/controller/thread/WorkerThread.java
@@ -1,6 +1,7 @@
 package it.polimi.template.controller.thread;
 
 import it.polimi.template.model.Mission;
+import it.polimi.template.model.Trip;
 import it.polimi.template.model.editor.*;
 
 public class WorkerThread implements Runnable {
@@ -10,21 +11,27 @@ public class WorkerThread implements Runnable {
 	private DroneAllocator da = new DroneAllocator();
 	private TripLauncher tl = new TripLauncher();
 	private TripMonitor tm = new TripMonitor();
-	
+
 	public WorkerThread(Mission mission) {
 		this.m = mission;
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		m = mc.run(m);
-		
-		m = da.run(m); 
 
-		m = tl.run(m); 
-		
-		m = tm.run(m);
+		while (m.getStatus() != Mission.COMPLETED) {
+
+			m = da.run(m);
+
+			m = tl.run(m);
+
+			m = tm.run(m);
+
+			if(m.getStatus() == Mission.FAILED)
+				break;
+		}
+
 	}
-
 }
