f8a574b36dc88f0a29fba8fb591fb88afd59f92f
alberto cardellini <albe.carde@gmail.com>
2015-01-07 16:39:05 +0100

    asdasd

--- src/it/polimi/template/controller/MonitorPageController.java
+++ src/it/polimi/template/controller/MonitorPageController.java
@@ -3,11 +3,14 @@ package it.polimi.template.controller;
 import it.polimi.template.model.Drone;
 import it.polimi.template.model.Item;
 import it.polimi.template.model.Mission;
+import it.polimi.template.model.Trip;
 import it.polimi.template.view.MonitorPage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
 
 public class MonitorPageController {
 
@@ -33,6 +36,35 @@ public class MonitorPageController {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Start the execution, the behaviour depends on the diagram generation
+//			for (Mission m : missions) {
+//
+//				dpsbl.sortTripsByPriority(m.getTrips());
+//				mpob.startDroneAllocator(m, drones);
+//				dpsbl.startTripLauncher(m);
+//				dpsbl.startTripMonitor(m);
+//
+//				for (Trip t : m.getTrips()) {
+//
+//					trips.add(t);
+//
+//					if (!t.getUsed()) {
+//
+//						DefaultTableModel model = (DefaultTableModel) table
+//								.getModel();
+//
+//						if (t.getDrone() == null)
+//							model.addRow(new Object[] { "", t.getName(),
+//									t.getStatus() });
+//						else
+//							model.addRow(new Object[] {
+//									t.getDrone().getId(), t.getName(),
+//									t.getStatus() });
+//						t.setUsed(true);
+//
+//					}
+//				}
+//
+//			}
 		}
 	}
 
