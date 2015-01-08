46ef7aefa55451be5ba29220355f80b236a087b7
Manuel <umanueuhotmail.it>
2015-01-08 16:35:33 +0100

    fsdf

--- src/it/polimi/template/model/editor/DroneAllocator.java
+++ src/it/polimi/template/model/editor/DroneAllocator.java
@@ -23,7 +23,7 @@ public class DroneAllocator implements Node {
 			for (Drone d : DronesManager.getDrones()) {
 				if (d.getStatus() == Drone.FREE
 						&& t.getStatus() != Trip.DELAYED) {
-					if (t.getItem().getShapeCategory() == d.getShapeCategory() || t.getItem() == null) {
+					if (t.getItem() == null ||t.getItem().getShapeCategory() == d.getShapeCategory()  ) {
 						d.setStatus(Drone.BUSY);
 						t.setDrone(d);
 
