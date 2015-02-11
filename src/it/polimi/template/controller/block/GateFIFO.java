b9cb1d03f25b1909baaf3257d55cfb02403f3196
alberto <albe.carde@gmail.com>
2015-02-11 15:26:29 +0100

    asdasd

--- src/it/polimi/template/controller/block/GateFIFO.java
+++ src/it/polimi/template/controller/block/GateFIFO.java
@@ -9,11 +9,12 @@ import java.util.Observer;
 public class GateFIFO extends Node implements Observer {
 
 	MissionWorker mw;
+	boolean flag = true;
 
 	public GateFIFO(MissionWorker mw) {
 		this.mw = mw;
 	}
-	
+
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		Mission m = this.run((Mission) arg1);
@@ -25,8 +26,14 @@ public class GateFIFO extends Node implements Observer {
 
 	@Override
 	public Mission run(Mission m) {
-		// TODO Auto-generated method stub
-		return m;
+
+		if (m != null && flag == true) {
+			flag = false;
+			return m;
+		}
+
+//		flag = true;
+		return null;
 	}
 
 }
