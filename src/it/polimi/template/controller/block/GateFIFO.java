321d5bc27b01d76e7584ada220ef21ebf0cddb1a
alberto <albe.carde@gmail.com>
2015-02-11 16:02:31 +0100

    doing the GateFIFO implementation

--- src/it/polimi/template/controller/block/GateFIFO.java
+++ src/it/polimi/template/controller/block/GateFIFO.java
@@ -2,6 +2,7 @@ package it.polimi.template.controller.block;
 
 import it.polimi.template.controller.thread.MissionWorker;
 import it.polimi.template.model.Mission;
+import it.polimi.template.model.Trip;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -9,7 +10,8 @@ import java.util.Observer;
 public class GateFIFO extends Node implements Observer {
 
 	MissionWorker mw;
-	boolean flag = true;
+	boolean flag = true; // control flag that denote if the first trip occurence is already passed
+	Trip lastTripPassed; // the trip occurence that passed as first one, among all trips incoming in this block
 
 	public GateFIFO(MissionWorker mw) {
 		this.mw = mw;
@@ -27,12 +29,18 @@ public class GateFIFO extends Node implements Observer {
 	@Override
 	public Mission run(Mission m) {
 
+		// if flag is false, it means that one of the multiple trip occurences is already passed
+		// if flaf is true, this is the first occurence coming in
 		if (m != null && flag == true) {
+			// set flag false so that other occurrences will not pass
 			flag = false;
+
+			// TODO
+			
 			return m;
 		}
 
-//		flag = true;
+		// flag = true;
 		return null;
 	}
 
