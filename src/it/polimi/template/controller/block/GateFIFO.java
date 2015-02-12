641e518e6443518a26faead7337a75d4f0410d9c
Alberto Cardellini <albe.carde@gmail.com>
2015-02-12 15:46:02 +0100

    asdasd

--- src/it/polimi/template/controller/block/GateFIFO.java
+++ src/it/polimi/template/controller/block/GateFIFO.java
@@ -10,8 +10,11 @@ import java.util.Observer;
 public class GateFIFO extends Node implements Observer {
 
 	MissionWorker mw;
-	Mission mBuffer = null;
-
+	boolean firstPassed = false;		 // instance of passed mission
+	int incomingConnections = 2; // incoming connections of the block, 
+								 // this value is generated dinamically by Pluto Graphical Editor
+	int counter = 0; 
+	
 	public GateFIFO(MissionWorker mw) {
 		this.mw = mw;
 	}
@@ -28,14 +31,23 @@ public class GateFIFO extends Node implements Observer {
 	@Override
 	public Mission run(Mission m) {
 		
-		if(mBuffer == null){
-			mBuffer = m;
+		// if no instance has been passed yet
+		if( !firstPassed ){
+			firstPassed = true; // set flag to true
+			counter++;
 			return m;
 		}
+		// else if not all parallel instances have been passed yet
+		else /*if(counter < incomingConnections)*/{
+			counter++;
+			// if this instance was the last one that had to be passed
+			if(counter == incomingConnections){
+				firstPassed = false; // set the flag to null
+			}
+			return null;
+		}
+//		return null;
 		
-		// if(tutte le istanze attese sono arrivate) -> mBuffer = null;
-		
-		return null;
 	}
 
 }
